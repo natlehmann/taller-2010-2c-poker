23f66d48ddb048ea8e209844237ff9173b9cb812
natlehmann <natlehmann@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-09-06 20:08:41 +0000

    implementacion parcial

--- Poker/Comun/XmlParserEstadoCerrandoInline.cpp
+++ Poker/Comun/XmlParserEstadoCerrandoInline.cpp
@@ -1,13 +1,16 @@
 
 #include "XmlParserEstadoCerrandoInline.h"
 
+
 XmlParserEstadoCerrandoInline::XmlParserEstadoCerrandoInline(deque<string*>* nodosProcesados) 
 	: XmlParserEstado(nodosProcesados)
 {
+	//this->inicial = new XmlParserEstadoInicial(nodosProcesados);
 }
 
 XmlParserEstadoCerrandoInline::~XmlParserEstadoCerrandoInline(void)
 {
+	//delete(this->inicial);
 }
 
 XmlParserEstado* XmlParserEstadoCerrandoInline::procesarFragmento() {
