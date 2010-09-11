3af7f1cd2cb5a302c5d25db162d1c0d0669be2b0
natlehmann <natlehmann@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-09-11 15:09:43 +0000

    primera implementacion (incompleta)

--- Poker/Servidor/ThrCliente.h
+++ Poker/Servidor/ThrCliente.h
@@ -3,24 +3,37 @@
 #include "Socket.h"
 #include "Thread.h"
 
+#include "Parser.h"
+#include "XmlParser.h"
+#include "DomTree.h"
+#include "FabricaOperaciones.h"
+#include "Operacion.h"
+
 
 class ThrCliente: public Thread
 {
 	private:
 		bool parar;
 		Socket* sock;
+		FabricaOperaciones* fabricaOperaciones;
 		
 	public:	
-		ThrCliente(){};
+		ThrCliente(){
+			this->fabricaOperaciones = new FabricaOperaciones();
+		};
 				
 		ThrCliente(Socket* sockCliente)
 		{
 			this->sock = sockCliente;
 			this->parar = false;
+			this->fabricaOperaciones = new FabricaOperaciones();
 
 		};
 		
-		virtual ~ThrCliente(){delete this->sock;};
+		virtual ~ThrCliente(){
+			delete (this->sock);
+			delete (this->fabricaOperaciones);
+		};
 		
 		void pararCliente()
 		{
@@ -35,20 +48,53 @@ class ThrCliente: public Thread
 			bool recibidoOK;
 			bool error = true;
 			
+			string msjAcumulado = "";
+
 			// Comienza la entrada y salida de personas
 			while (!this->parar)
 			{
 				// Se espera el envio del cliente
 				recibidoOK = sock->recibir(msjRecibido);
-				cout << msjRecibido;
+
+				msjAcumulado = msjAcumulado + msjRecibido;
+
+				if (!recibidoOK) {
+					this->parar = true;
+				}
+			}
+
+			cout << "VA MENSAJE TOTAL = " << endl << msjAcumulado << endl;
+			try {
+				Parser* parser = new XmlParser();
+				DomTree* arbol = parser->toDom(msjAcumulado);
+
+				Operacion* operacion = this->fabricaOperaciones->newOperacion(arbol);
+
+				// TODO : enviar operacion e generador de respuestas
+				// eso tiene que generar tanto resultado como error
+				cout << operacion->getId() << " " << endl;
+
+
+
+			} catch (PokerException& e) {
+				// TODO 
+				//////
+				cout << e.what() << endl;
+			}
+
+
 				// Se envia la respuesta correspondiente
 				msjRecibido = "Respuesta de " + msjRecibido;
 				
+				/*
 				if(!sock->enviar(msjRecibido, msjRecibido.length()))
 				{
 					this->pararCliente();
 				}
-			}
+				*/
+
+				
+
 						
 			// Se desconecta el socket
 			sock->cerrar();
