cffb79648eba49170d85d14c3c2e4515d8c11021
natlehmann <natlehmann@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-09-13 00:56:53 +0000

    se agrega numero de linea en errores

--- trunk/Poker/Servidor/FabricaOperaciones.cpp
+++ trunk/Poker/Servidor/FabricaOperaciones.cpp
@@ -6,6 +6,7 @@
 #include "UtilTiposDatos.h"
 #include "DatosInvalidosException.h"
 #include "Error.h"
+#include "MensajesUtil.h"
 #include <list>
 using namespace std;
 
@@ -49,18 +50,26 @@ Operacion* FabricaOperaciones::newOperacion(DomTree* domTree)
 		throw DatosInvalidosException(resultado);
 	}
 	if (strcmp("pedido",elementoPedido->getNombre().c_str()) != 0) {
-		Error resultado("V","Se esperaba tag 'pedido' y se encontro '"+elementoPedido->getNombre()+"'.","");
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoPedido->getNumeroDeLinea()) 
+			+  string(". Se esperaba tag 'pedido' y se encontro '") 
+			+ elementoPedido->getNombre() + "'.","");
 		throw DatosInvalidosException(resultado);
 	}
 	if (elementoPedido->getTexto().size() != 0) {
-		Error resultado("V","El tag 'pedido' no puede contener texto. Se encontro '"+elementoPedido->getTexto()+"'.","");
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoPedido->getNumeroDeLinea()) 
+			+ string(". El tag 'pedido' no puede contener texto. Se encontro '")
+			+ elementoPedido->getTexto()+"'.","");
 		throw DatosInvalidosException(resultado);
 	}
 
 	list<Elemento*>* hijosPedido = elementoPedido->getHijos();
 
 	if (hijosPedido == NULL || hijosPedido->size() != 2) {
-		Error resultado("V","El tag 'pedido' debe tener dos hijos, 'operacion' y 'parametros'.","");
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoPedido->getNumeroDeLinea()) 
+			+ string(". El tag 'pedido' debe tener dos hijos, 'operacion' y 'parametros'."),"");
 		throw DatosInvalidosException(resultado);
 	}
 
@@ -68,22 +77,32 @@ Operacion* FabricaOperaciones::newOperacion(DomTree* domTree)
 	Elemento* elementoOperacion = *it;
 
 	if (strcmp("operacion",elementoOperacion->getNombre().c_str()) != 0) {
-		Error resultado("V","Se esperaba tag 'operacion' y se encontro '"+elementoOperacion->getNombre()+"'.","");
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoOperacion->getNumeroDeLinea()) 
+			+ string(". Se esperaba tag 'operacion' y se encontro '")
+			+ elementoOperacion->getNombre() + "'.","");
 		throw DatosInvalidosException(resultado);
 	}
 	if (elementoOperacion->getAtributos()->size() != 1) {
-		Error resultado("V","El tag 'operacion' debe tener un atributo 'id'.","");
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoOperacion->getNumeroDeLinea())
+			+ string(". El tag 'operacion' debe tener un atributo 'id'."),"");
 		throw DatosInvalidosException(resultado);
 	}
 
 	idOperacion = elementoOperacion->getAtributo("id");
 
 	if (idOperacion.length() != 1 || (idOperacion[0] != 'S' && idOperacion[0] != 'R' && idOperacion[0] != 'M' && idOperacion[0] != 'D')) {
-		Error resultado("V","El atributo 'id' del tag 'operacion' debe ser 'S', 'R', 'M' o 'D'.","");
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoOperacion->getNumeroDeLinea())
+			+ string(". El atributo 'id' del tag 'operacion' debe ser 'S', 'R', 'M' o 'D'."),"");
 		throw DatosInvalidosException(resultado);
 	}
 	if (elementoOperacion->getTexto().size() != 0) {
-		Error resultado("V","El tag 'operacion' no puede contener texto. Se encontro '"+elementoOperacion->getTexto()+"'.",idOperacion);
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoOperacion->getNumeroDeLinea())
+			+ string(". El tag 'operacion' no puede contener texto. Se encontro '")
+			+ elementoOperacion->getTexto()+"'.",idOperacion);
 		throw DatosInvalidosException(resultado);
 	}
 
@@ -91,22 +110,32 @@ Operacion* FabricaOperaciones::newOperacion(DomTree* domTree)
 	Elemento* elementoParametros = *it;
 
 	if (strcmp("parametros",elementoParametros->getNombre().c_str()) != 0) {
-		Error resultado("V","Se esperaba tag 'parametros' y se encontro '"+elementoParametros->getNombre()+"'.",idOperacion);
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoParametros->getNumeroDeLinea())
+			+ string(". Se esperaba tag 'parametros' y se encontro '")
+			+ elementoParametros->getNombre()+"'.",idOperacion);
 		throw DatosInvalidosException(resultado);
 	}
 	if (elementoParametros->getTexto().size() != 0) {
-		Error resultado("V","El tag 'parametros' no puede contener texto y se encontro '"+elementoParametros->getTexto()+"'.",idOperacion);
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoParametros->getNumeroDeLinea())
+			+ string(". El tag 'parametros' no puede contener texto y se encontro '")
+			+ elementoParametros->getTexto()+"'.",idOperacion);
 		throw DatosInvalidosException(resultado);
 	}
 
 	list<Elemento*>* hijosParametros = elementoParametros->getHijos();
 
 	if (hijosParametros == NULL || hijosParametros->size() == 0) {
-		Error resultado("V","El tag 'parametros' no tiene hijos. Se esperaba tag 'parametro'.",idOperacion);
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoParametros->getNumeroDeLinea())
+			+ string(". El tag 'parametros' no tiene hijos. Se esperaba tag 'parametro'."),idOperacion);
 		throw DatosInvalidosException(resultado);
 	}
 	if (idOperacion[0] == 'D' && hijosParametros->size() != 2) {
-		Error resultado("V","Una division debe tener 2 parametros, 'dividendo' y 'divisor'.",idOperacion);
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(elementoParametros->getNumeroDeLinea())
+			+ string(". Una division debe tener 2 parametros, 'dividendo' y 'divisor'."),idOperacion);
 		throw DatosInvalidosException(resultado);
 	}
 
@@ -115,7 +144,9 @@ Operacion* FabricaOperaciones::newOperacion(DomTree* domTree)
 		cantidadParametros++;
 		Elemento* elementoParametro = *it;
 		if (elementoParametro == NULL) {
-			Error resultado("V","El tag 'parametros' no tiene hijos. Se esperaba tag 'parametro'.",idOperacion);
+			Error resultado("V",
+				string("Error en linea ") + MensajesUtil::intToString(elementoParametros->getNumeroDeLinea())
+				+ string(". El tag 'parametros' no tiene hijos. Se esperaba tag 'parametro'."),idOperacion);
 			throw DatosInvalidosException(resultado);
 		}
 		validarParametro(elementoParametro, idOperacion, cantidadParametros);
@@ -145,7 +176,9 @@ Operacion* FabricaOperaciones::newOperacion(DomTree* domTree)
 void FabricaOperaciones::validarParametro(Elemento* parametro, string idOperacion, int numeroDeParametro)
 {
 	if (parametro->getAtributos()->size() != 1) {
-		Error resultado("V","El tag 'parametro' debe tener un atributo 'nombre'.","");
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(parametro->getNumeroDeLinea())
+			+ string(". El tag 'parametro' debe tener un atributo 'nombre'."),"");
 		throw DatosInvalidosException(resultado);
 	}
 
@@ -153,21 +186,30 @@ void FabricaOperaciones::validarParametro(Elemento* parametro, string idOperacio
 	{
 	case 'S':
 		if (strcmp("sum",parametro->getAtributo("nombre").c_str()) != 0) {
-			Error resultado("V","El atributo 'nombre' del tag 'parametro' para una suma debe ser 'sum'. Se encontro '"+parametro->getAtributo("nombre")+"'.",idOperacion);
+			Error resultado("V",
+				string("Error en linea ") + MensajesUtil::intToString(parametro->getNumeroDeLinea())
+				+ string(". El atributo 'nombre' del tag 'parametro' para una suma debe ser 'sum'. Se encontro '")
+				+ parametro->getAtributo("nombre")+"'.",idOperacion);
 			throw DatosInvalidosException(resultado);
 		}
 		break;
 
 	case 'R':
 		if (strcmp("res",parametro->getAtributo("nombre").c_str()) != 0) {
-			Error resultado("V","El atributo 'nombre' del tag 'parametro' para una resta debe ser 'res'. Se encontro '"+parametro->getAtributo("nombre")+"'.",idOperacion);
+			Error resultado("V",
+				string("Error en linea ") + MensajesUtil::intToString(parametro->getNumeroDeLinea())
+				+ string(". El atributo 'nombre' del tag 'parametro' para una resta debe ser 'res'. Se encontro '")
+				+ parametro->getAtributo("nombre")+"'.",idOperacion);
 			throw DatosInvalidosException(resultado);
 		}
 		break;
 
 	case 'M':
 		if (strcmp("mul",parametro->getAtributo("nombre").c_str()) != 0) {
-			Error resultado("V","El atributo 'nombre' del tag 'parametro' para una multiplicacion debe ser 'mul'. Se encontro '"+parametro->getAtributo("nombre")+"'.",idOperacion);
+			Error resultado("V",
+				string("Error en linea ") + MensajesUtil::intToString(parametro->getNumeroDeLinea())
+				+ string(". El atributo 'nombre' del tag 'parametro' para una multiplicacion debe ser 'mul'. Se encontro '")
+				+ parametro->getAtributo("nombre")+"'.",idOperacion);
 			throw DatosInvalidosException(resultado);
 		}
 		break;
@@ -175,12 +217,18 @@ void FabricaOperaciones::validarParametro(Elemento* parametro, string idOperacio
 	case 'D':
 		if (numeroDeParametro == 1) {
 			if (strcmp("dividendo",parametro->getAtributo("nombre").c_str()) != 0) {
-				Error resultado("V","El primer parametro para una division debe ser tener el valor 'dividendo' en el atributo 'nombre' . Se encontro '"+parametro->getAtributo("nombre")+"'.",idOperacion);
+				Error resultado("V",
+					string("Error en linea ") + MensajesUtil::intToString(parametro->getNumeroDeLinea())
+					+ string(". El primer parametro para una division debe ser tener el valor 'dividendo' en el atributo 'nombre' . Se encontro '")
+					+ parametro->getAtributo("nombre")+"'.",idOperacion);
 				throw DatosInvalidosException(resultado);
 			}
 		} else {
 			if (strcmp("divisor",parametro->getAtributo("nombre").c_str()) != 0) {
-				Error resultado("V","El segundo parametro para una division debe ser tener el valor 'divisor' en el atributo 'nombre' . Se encontro '"+parametro->getAtributo("nombre")+"'.",idOperacion);
+				Error resultado("V",
+					string("Error en linea ") + MensajesUtil::intToString(parametro->getNumeroDeLinea())
+					+ string(". El segundo parametro para una division debe ser tener el valor 'divisor' en el atributo 'nombre' . Se encontro '")
+					+ parametro->getAtributo("nombre")+"'.",idOperacion);
 				throw DatosInvalidosException(resultado);
 			}
 		}
@@ -188,7 +236,10 @@ void FabricaOperaciones::validarParametro(Elemento* parametro, string idOperacio
 	}
 
 	if (!UtilTiposDatos::isNumber(parametro->getTexto())) {
-		Error resultado("V","El parametro contiene un valor que no es numerico. Se encontro '"+parametro->getTexto()+"'.",idOperacion);
+		Error resultado("V",
+			string("Error en linea ") + MensajesUtil::intToString(parametro->getNumeroDeLinea())
+			+ string(". El parametro contiene un valor que no es numerico. Se encontro '")
+			+ parametro->getTexto()+"'.",idOperacion);
 		throw DatosInvalidosException(resultado);
 	}
 }
