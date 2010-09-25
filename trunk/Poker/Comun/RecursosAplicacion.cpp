335c5eb4883ff7c9288ca96485996d4159977213
natlehmann@gmail.com <natlehmann@gmail.com@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-09-25 20:25:33 +0000

    se agregan logs

--- trunk/Poker/Comun/RecursosAplicacion.cpp
+++ trunk/Poker/Comun/RecursosAplicacion.cpp
@@ -1,6 +1,12 @@
 #include "RecursosAplicacion.h"
 
+// inicializacion de miembros estaticos
 Properties RecursosAplicacion::parserConfigProperties("..\\Comun\\parserConfig.properties");
+Properties RecursosAplicacion::clienteConfigProperties("..\\Cliente\\config.ini");
+Properties RecursosAplicacion::servidorConfigProperties("..\\Servidor\\config.ini");
+
+Log RecursosAplicacion::logErroresCliente("..\\logs\\errores.err");
+Log RecursosAplicacion::logErroresServidor("..\\logs\\serv-errores.err");
 
 RecursosAplicacion::RecursosAplicacion(void)
 {
@@ -13,6 +19,22 @@ RecursosAplicacion::~RecursosAplicacion(void)
 
 Properties* RecursosAplicacion::getParserConfigProperties(){
 	return &parserConfigProperties;
+}
+
+Properties* RecursosAplicacion::getClienteConfigProperties(){
+	return &clienteConfigProperties;
+}
+
+Properties* RecursosAplicacion::getServidorConfigProperties(){
+	return &servidorConfigProperties;
+}
+
+Log* RecursosAplicacion::getLogErroresCliente(){
+	return &logErroresCliente;
+}
+
+Log* RecursosAplicacion::getLogErroresServidor(){
+	return &logErroresServidor;
 }
 
 
