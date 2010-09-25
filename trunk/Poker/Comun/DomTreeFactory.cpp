a70339af23a0d654acc2ffb7d4b5ffebdd80c797
natlehmann@gmail.com <natlehmann@gmail.com@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-09-25 12:15:24 +0000

    se modifica para soportar multiples configuraciones de archivos

--- trunk/Poker/Comun/DomTreeFactory.cpp
+++ trunk/Poker/Comun/DomTreeFactory.cpp
@@ -5,94 +5,85 @@
 #include "ArrayUtil.h"
 #include "MensajesUtil.h"
 #include "ParserException.h"
+#include "DomTreeFactoryInstance.h"
+#include "RecursosAplicacion.h"
+#include "Properties.h"
+
+
+// Inicializacion de Miembros estaticos
+map<string, DomTreeFactoryInstance> DomTreeFactory::configuraciones;
+string DomTreeFactory::defaultConfig = "";
+
 
 DomTreeFactory::DomTreeFactory(void)
 {
 }
 
-DomTreeFactory::~DomTreeFactory(void)
-{
+DomTreeFactory* DomTreeFactory::config(string nombreConfig){
+
+	if (DomTreeFactory::configuraciones.empty()) {
+		DomTreeFactory::inicializar();
+	}
+
+	map<string, DomTreeFactoryInstance>::iterator it = DomTreeFactory::configuraciones.find(nombreConfig);
+	if (it != DomTreeFactory::configuraciones.end()) {
+		return &(it->second);
+	
+	} else {
+		throw ParserException("La configuracion del parser " + nombreConfig + " no es valida.", "V");
+	}
 }
+
+void DomTreeFactory::inicializar(){
+
+	Properties* prop = RecursosAplicacion::getParserConfigProperties();
+	string configs = prop->get("parser.configs");
+	list<string> listaConfigs = MensajesUtil::split(configs);
+
+	for (list<string>::iterator it = listaConfigs.begin(); it != listaConfigs.end(); it++) {
+
+		DomTreeFactoryInstance instance(*it);
+
+		string elementos = prop->get("parser." + (*it) + ".elementos");
+		list<string> listaElementos = MensajesUtil::split(elementos);
+		instance.setElementos(listaElementos);
+
+		map<string,string> hijosPorElemento;
+		map<string,string> attPorElemento;
+
+		for (list<string>::iterator itElem = listaElementos.begin(); 
+			itElem != listaElementos.end(); itElem++) {
+
+				string hijos = prop->get("parser." + (*it) + "." + (*itElem) + ".hijos");
+				hijosPorElemento.insert(pair<string,string>((*itElem), hijos));
+
+				string atributos = prop->get("parser." + (*it) + "." + (*itElem) + ".atributos");
+				attPorElemento.insert(pair<string,string>((*itElem), atributos));
+		}
+
+		instance.setHijosPorElemento(hijosPorElemento);
+		instance.setAttPorElemento(attPorElemento);
+
+		DomTreeFactory::configuraciones.insert(pair<string,DomTreeFactoryInstance>((*it), instance));
+	}
+
+	DomTreeFactory::defaultConfig = prop->get("parser.default.config");
+}
+
 
 Elemento* DomTreeFactory::crearElemento(string nombre) {
 
-	Elemento* elemento;
-
-	//TODO: Se podrìa reemplazar por configuraciòn de un archivo
-	if (MensajesUtil::sonIguales(nombre, string("raiz"))) {
-		char* hijos[] = {"pedido","respuesta","\0"};
-		char* atributos[] = {"\0"};
-		elemento = configurarNodo(nombre, hijos, atributos);
+	if (DomTreeFactory::configuraciones.empty()) {
+		DomTreeFactory::inicializar();
 	}
 
-	if (MensajesUtil::sonIguales(nombre, string("pedido"))) {
-		char* hijos[] = {"operacion","parametros","\0"};
-		char* atributos[] = {"\0"};
-		elemento = configurarNodo(nombre, hijos, atributos);
-	}
-
-	if (MensajesUtil::sonIguales(nombre, string("operacion"))) {
-		char* hijos[] = {"\0"};
-		char* atributos[] = {"id","\0"};
-		elemento = configurarNodo(nombre, hijos, atributos);
-	}
-
-	if (MensajesUtil::sonIguales(nombre, string("parametros"))) {
-		char* hijos[] = {"parametro", "\0"};
-		char* atributos[] = {"\0"};
-		elemento = configurarNodo(nombre, hijos, atributos);
-	}
-
-	if (MensajesUtil::sonIguales(nombre, string("parametro"))) {
-		char* hijos[] = {"\0"};
-		char* atributos[] = {"nombre", "\0"};
-		elemento = configurarNodo(nombre, hijos, atributos);
-	}
-
-	if (MensajesUtil::sonIguales(nombre, string("respuesta"))) {
-		char* hijos[] = {"operacion", "resultados", "errores", "\0"};
-		char* atributos[] = {"\0"};
-		elemento = configurarNodo(nombre, hijos, atributos);
-	}
-
-	if (MensajesUtil::sonIguales(nombre, string("resultados"))) {
-		char* hijos[] = {"resultado", "\0"};
-		char* atributos[] = {"\0"};
-		elemento = configurarNodo(nombre, hijos, atributos);
-	}
-
-	if (MensajesUtil::sonIguales(nombre, string("resultado"))) {
-		char* hijos[] = {"\0"};
-		char* atributos[] = {"nombre", "\0"};
-		elemento = configurarNodo(nombre, hijos, atributos);
-	}
-
-	if (MensajesUtil::sonIguales(nombre, string("errores"))) {
-		char* hijos[] = {"error", "\0"};
-		char* atributos[] = {"\0"};
-		elemento = configurarNodo(nombre, hijos, atributos);
-	}
-
-	if (MensajesUtil::sonIguales(nombre, string("error"))) {
-		char* hijos[] = {"\0"};
-		char* atributos[] = {"tipo", "\0"};
-		elemento = configurarNodo(nombre, hijos, atributos);
-	}
-
-	if (elemento != NULL) {		
-		return elemento;
-
-	} else {
-		string msg = "El elemento " + nombre + " no esta permitido.";
-		throw ParserException(msg, "V");
-	}
-	
+	return DomTreeFactory::config(DomTreeFactory::defaultConfig)->elemento(nombre);	
 }
 
-Elemento* DomTreeFactory::configurarNodo(string nombre, char* hijos[], char* atributos[]){
 
-	Elemento* elemento = new Elemento(nombre);
-	elemento->setHijosPermitidos(ArrayUtil::charArrayToSet(hijos));
-	elemento->setAtributosPermitidos(ArrayUtil::charArrayToSet(atributos));
-	return elemento;
+// Este metodo debe ser redefinido en las subclases
+Elemento* DomTreeFactory::elemento(string nombre){
+	return NULL;
 }
+
+
