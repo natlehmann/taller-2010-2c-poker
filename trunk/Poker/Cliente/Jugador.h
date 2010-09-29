f746b05e5a8ae1422b46c5f6c6a5ff2a4814eca9
natlehmann@gmail.com <natlehmann@gmail.com@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-09-29 03:32:44 +0000

    agregado

---
+++ trunk/Poker/Cliente/Jugador.h
@@ -0,0 +1,44 @@
+#ifndef _JUGADOR_H__
+#define _JUGADOR_H__
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+#include "ElementoGrafico.h"
+#include "Imagen.h"
+
+#define ALTO_MIN_ETIQUETA	0.25
+#define ANCHO_MIN_CARTAS	0.3
+
+class Jugador : public ElementoGrafico {
+private:
+	int id;
+	string nombre;
+	int posicion;
+	Imagen* imagen;
+
+	void setearDisposicionAIzq();
+	void setearDisposicionADer();
+	SDL_Rect* calcularRectFoto();
+
+public:
+	Jugador(void);
+	virtual ~Jugador(void);
+
+	virtual void dibujar();
+
+	int getId();
+	void setId(int id);
+	string getNombre();
+	void setNombre(string nombre);
+	int getPosicion();
+	void setPosicion(int posicion);
+
+	Imagen* getImagen();
+	void setImagen(Imagen* imagen);
+};
+
+#endif //_JUGADOR_H__
---
+++ trunk/Poker/Cliente/ServiciosGraficos.h
@@ -0,0 +1,43 @@
+#ifndef _SERVICIOSGRAFICOS_H__
+#define _SERVICIOSGRAFICOS_H__
+
+#include "SDL.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+#define CELDAS_HORIZ	6
+#define CELDAS_VERT		3
+
+class ServiciosGraficos {
+private:
+	static const SDL_VideoInfo* videoInfo;
+
+	/**
+	* Este metodo requiere que previamente se bloquee la pantalla
+	*/
+	static void putPixel(SDL_Surface* superficie, int x, int y, Uint32 pixel);
+
+public:
+	ServiciosGraficos(void);
+	virtual ~ServiciosGraficos(void);
+
+	/**
+	* El parametro offset indica el punto superior izquierdo desde el cual se comienza a dibujar
+	* el circulo. Este parametro debe tener seteados los atributos w y h, ya que estos
+	* se evaluaran para fijar el tamaño de la elipse.
+	*/
+	static void dibujarElipse(SDL_Surface* superficie, 
+		SDL_Rect* offset, Uint8 red, Uint8 green, Uint8 blue);
+
+	static const SDL_VideoInfo* getVideoInfo();
+
+	static int getTamanioCeldaHoriz();
+	static int getTamanioCeldaVert();
+
+	static int getAnchoVentana();
+	static int getAltoVentana();
+};
+
+#endif //_SERVICIOSGRAFICOS_H__
