42e6644f3dcedf6c2bf87778b994e59716349b57
natlehmann@gmail.com <natlehmann@gmail.com@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-10-09 14:59:08 +0000

    se eliminan comentarios

--- Poker/Cliente/OpUIClienteSolicitarEscenario.cpp
+++ Poker/Cliente/OpUIClienteSolicitarEscenario.cpp
@@ -40,39 +40,6 @@ void OpUIClienteSolicitarEscenario::ejecutar(Ventana* ventana){
 	try {
 		DomTree* arbolEscenario = parser->toDom(msjRecibido, "escenario");
 		FabricaDeElementosGraficos::generarEscenario(arbolEscenario, ventana);
-
-		// TODO: ELIMINAR ESTO (ES EJEMPLO, SE DEBERIA INSTANCIAR EN LA FABRICA)
-		//Mesa* mesa = new Mesa();
-		//mesa->setAlto(ventana->getAlto());
-		//mesa->setAncho(ventana->getAncho());
-		//mesa->setFondo("..\\Cliente\\recursos\\imagenes\\mesa600x800.bmp");
-		//ventana->agregarElementoGrafico(mesa);
-		//Jugador* jugador = new Jugador();
-		//jugador->setImagen(new Imagen("..\\Servidor\\recursos\\imagenes\\jugador1.bmp"));
-		//jugador->setPosicion(1);
-		//ventana->agregarElementoGrafico(jugador);
-		/*Jugador* jugador2 = new Jugador();
-		jugador2->setImagen(new Imagen("..\\Servidor\\recursos\\imagenes\\jugador2.bmp"));
-		jugador2->setPosicion(2);
-		ventana->agregarElementoGrafico(jugador2);
-		Jugador* jugador3 = new Jugador();
-		jugador3->setImagen(new Imagen("..\\Servidor\\recursos\\imagenes\\jugador3.bmp"));
-		jugador3->setPosicion(3);
-		ventana->agregarElementoGrafico(jugador3);
-		Jugador* jugador4 = new Jugador();
-		jugador4->setImagen(new Imagen("..\\Servidor\\recursos\\imagenes\\jugador4.bmp"));
-		jugador4->setPosicion(4);
-		ventana->agregarElementoGrafico(jugador4);
-		Jugador* jugador5 = new Jugador();
-		jugador5->setImagen(new Imagen("..\\Servidor\\recursos\\imagenes\\jugador1.bmp"));
-		jugador5->setPosicion(5);
-		ventana->agregarElementoGrafico(jugador5);
-		Jugador* jugador6 = new Jugador();
-		jugador6->setImagen(new Imagen("..\\Servidor\\recursos\\imagenes\\jugador2.bmp"));
-		jugador6->setPosicion(6);
-		ventana->agregarElementoGrafico(jugador6);*/
-		// HASTA ACA
-
 		delete (arbolEscenario);
 
 	} catch (PokerException& e) {
