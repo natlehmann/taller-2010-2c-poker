97e66200ee00c59724815f28f5388a8672713742
luchopal <luchopal@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-10-09 21:51:21 +0000

    Se modifico la clase Bote

--- Poker/Cliente/Bote.cpp
+++ Poker/Cliente/Bote.cpp
@@ -1,7 +1,15 @@
 #include "Bote.h"
-
+#include <sstream>
 Bote::Bote(void)
 {
+	int anchoSeccion = ServiciosGraficos::getTamanioCeldaHoriz();
+	int altoSeccion = ServiciosGraficos::getTamanioCeldaVert();
+	this->setAncho((int)(anchoSeccion*RELACION_ANCHO));
+	this->setAlto((int)(altoSeccion*RELACION_ALTO));
+	int posicionRelativaX = (anchoSeccion - this->ancho)/2;
+	int posicionRelativaY = (altoSeccion - this->alto)/2;
+	this->posX = ServiciosGraficos::getBordeIzquierdo() + ServiciosGraficos::getTamanioCeldaHoriz() * 2 + posicionRelativaX;
+	this->posY = ServiciosGraficos::getBordeSuperior() + posicionRelativaY;
 	this->imagen = NULL;
 	this->etiqueta = NULL;
 	this->total = 0;
@@ -15,6 +23,35 @@ Bote::~Bote(void)
 
 void Bote::dibujarSobreSup(SDL_Surface* superficie)
 {
+	if (this->imagen == NULL) {
+		this->imagen = new Imagen(
+			RecursosAplicacion::getClienteConfigProperties()->get(
+			"cliente.tema.default.imagen.bote"));
+	}
+
+//	this->imagen = new Imagen(nombreImagen);
+	int altoEtiqueta = (int)(this->getAlto() * BOTE_ALTO_ETIQUETA);
+
+	if (this->etiqueta == NULL) {
+		this->etiqueta = new Etiqueta();
+	}
+
+	this->etiqueta->setPosX(this->getPosX());
+	this->etiqueta->setPosY(this->getPosY());
+	this->etiqueta->setAncho(this->getAncho());
+	this->etiqueta->setAlto(altoEtiqueta);
+
+	std::stringstream mensaje;
+	mensaje << this->getTotal();
+	this->etiqueta->setMensaje(mensaje.str());
+
+	this->imagen->setPosX(this->getPosX());
+	this->imagen->setPosY(this->getPosY()+ altoEtiqueta);
+	this->imagen->setAncho(this->getAncho());
+	this->imagen->setAlto(this->getAlto());
+
+	this->imagen->dibujar(superficie);
+	this->etiqueta->dibujar(superficie);
 }
 
 int Bote::getTotal() 
--- Poker/Cliente/Bote.h
+++ Poker/Cliente/Bote.h
@@ -1,6 +1,12 @@
 #include "ElementoGrafico.h"
 #include "Imagen.h"
 #include "Etiqueta.h"
+#include "ServiciosGraficos.h"
+#include "RecursosAplicacion.h"
+
+#define BOTE_ALTO_ETIQUETA 0.18
+#define RELACION_ANCHO 0.8
+#define RELACION_ALTO 0.5
 
 class Bote: public ElementoGrafico
 {
