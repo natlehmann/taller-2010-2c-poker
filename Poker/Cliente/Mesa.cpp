64efe3d6edf73b161d4a49afdde2c8ec07637532
natlehmann@gmail.com <natlehmann@gmail.com@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-10-10 16:29:13 +0000

    se eliminan comentarios

--- Poker/Cliente/Mesa.cpp
+++ Poker/Cliente/Mesa.cpp
@@ -29,7 +29,6 @@ void Mesa::setFondo(string nombreImagen){
 		delete (this->imagen);
 	}
 
-	// TODO: VER SI PRIMERO TENEMOS QUE CALCULAR EL RESIZING DE LA IMAGEN
 	this->imagen = new Imagen(nombreImagen);
 	this->imagen->setAlto(this->getAlto());
 	this->imagen->setAncho(this->getAncho());
