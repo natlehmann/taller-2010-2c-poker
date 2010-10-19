04e6fc6bd0991d4354d4211559cccc02fb14abf1
natlehmann@gmail.com <natlehmann@gmail.com@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-10-19 23:47:00 +0000

    chequeo de cambios antes de dibujar

--- Poker/Cliente/Boton.h
+++ Poker/Cliente/Boton.h
@@ -8,6 +8,7 @@
 
 #include "ElementoGrafico.h"
 #include "Imagen.h"
+#include "Fuente.h"
 
 #define MAXIMO_ANCHO_PANTALLA_PROP_TXT 800
 #define PADDING_HORIZ   20
@@ -21,21 +22,27 @@ private:
 	Imagen* imagenUp;
 	Imagen* imagenDown;
 	Imagen* imagenOver;
+	Fuente* fuente;
+	bool habilitado;
+
+	string idOperacion;
 
 	char estado;
 
 	Imagen* setearImagen(string nombreImagen);
-	void setearPosicion();
+	//void setearPosicion();
+	void setearFuente();
 
 
 protected:
 	virtual void dibujarSobreSup(SDL_Surface* superficie);
 
 public:
-	Boton(void);
+	//Boton(void);
 	Boton(string texto);
-	Boton(string texto, int posx, int posy);
-	Boton(string texto, int ancho, int alto, int posx, int posy);
+	Boton(Boton* orig);
+	//Boton(string texto, int posx, int posy);
+	//Boton(string texto, int ancho, int alto, int posx, int posy);
 	virtual ~Boton(void);
 
 	string getTexto();
@@ -44,12 +51,23 @@ public:
 	bool esClickIzquierdo();
 	bool esClickDerecho();
 
+	bool isHabilitado();
+	void setHabilitado(bool habilitado);
+
+	string getIdOperacion();
+	void setIdOperacion(string idOperacion);
+
 	void dibujarOver(SDL_Surface* superficie);
 	void dibujarDown(SDL_Surface* superficie);
 	void dibujarUp(SDL_Surface* superficie);
 
 	bool checkClick(SDL_Surface* superficie);
 	bool checkOver(SDL_Surface* superficie);
+
+	virtual void setPosX(int posX);
+	virtual void setPosY(int posY);
+
+	virtual bool equals(ElementoGrafico* otro);
 };
 
 #endif //_BOTON_H__
