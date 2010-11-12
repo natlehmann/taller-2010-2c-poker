5bb5d78381212193ecfb18160795d28ca9a6ebca
luchopal <luchopal@a9434d28-8610-e991-b0d0-89a272e3a296>
2010-11-12 02:53:29 +0000

    Se agrego la clase VentanaEstadisticas.h

---
+++ trunk/Poker/Cliente/VentanaEstadisticas.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "VentanaBase.h"
+#include "SDL.h" 
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include <list>
+#include "Panel.h"
+#include "Boton.h"
+#include <windows.h>
+#include "Mensaje.h"
+
+#define SELECCION_MAYUSCULA "X"
+#define SELECCION "x"
+#define DIAS 31
+#define MESES 12
+#define	MINIMO_AÑOS 1900
+#define MAXIMO_AÑOS 9999
+
+class VentanaEstadisticas :
+	public VentanaBase
+{
+private:
+	TextBox* textboxFecha;
+	TextBox* textboxEvolUser;
+	TextBox* textboxEvolUserConnect;
+	
+	TextBox* textboxListUserReg;
+	TextBox* textboxListUserCon;
+	TextBox* textboxRanking;
+
+	Etiqueta* mensaje;
+
+	list<ElementoGrafico*> elementos;
+	list<ComponentePanel*> componentes;
+	
+	void configurarControles();
+	void refrescar(SDL_Surface* superficie);
+	bool manejarEventos(SDL_Event* event);
+	bool ejecutarEvento(string controlId);
+	void lanzarEvento(int codigoEvento);
+
+	bool validarFecha(string fecha);
+	bool validarSeleccion(string texto);
+	bool seleccionUnaOpciónOK();
+
+protected:
+	void dibujar(SDL_Surface* superficie);
+	virtual void dibujarSobreSup(SDL_Surface* superficie);
+public:
+	VentanaEstadisticas(void);
+	~VentanaEstadisticas(void);
+
+	virtual void iniciar();
+
+	virtual void agregarElementoGrafico(ElementoGrafico* elemento);
+
+	void agregarComponentePanel(ComponentePanel* componente);
+	
+	virtual void mostrarMensaje(string mensaje);
+};
